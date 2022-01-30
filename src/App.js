c90d6753636b66d58de933aff3ed54c8aa347fe9
joe mcgee <jrmcgee@asu.edu>
2022-01-30 14:35:18 +0900

    react code

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+    
+        <h1> Learn with Leon & Friends</h1>
+       
+        <h4> About Us<joe class = "black">|</joe><joe class = "green">Portfolio</joe></h4>
+        <img src={logo} className="leon" alt="logo" />
+       <text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequeat. Duis aute irure dolor in reprehenderit in voluptate
+           velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qu
+           i officia deserunt mollit anim id est laborum.</text>
+       
+           <p>
+           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+           incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+           nisi ut aliquip ex ea commodo consequeat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+            fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+           
+      </header>
+    
+    </div>
+  
+  );
+}
+
+export default App;
---
+++ src/footer.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+
+export default function Footer(){
+    return(
+<ul>
+<li>
+    Twitch 
+</li>
+    <li>
+        |
+    </li>
+    <li>
+    Twitter 
+</li>
+<li>
+    |
+</li>
+<li>
+    Youtube 
+</li>
+</ul>
+    )
+}
