810dfb7e00fc87a6a1748af72a278192fd8ebab4
joe mcgee <jrmcgee@asu.edu>
2022-02-02 15:17:52 +0900

    added effects

--- src/Body.js
+++ src/Body.js
@@ -1,11 +1,14 @@
 
 import './App.css';
 import logo from './logo.png';
+import Fade from 'react-reveal/Fade';
+import React from 'react';
 export default function Body(){
     return(
         <div>
 <h4> About Us<joe class = "black">|</joe><joe class = "green">Portfolio</joe></h4>
 <img src={logo} className="leon" alt="logo" />
+<Fade top>
 <text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
   ullamco laboris nisi ut aliquip ex ea commodo consequeat. Duis aute irure dolor in reprehenderit in voluptate
@@ -17,6 +20,7 @@ export default function Body(){
    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
    nisi ut aliquip ex ea commodo consequeat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
     fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </Fade>
         </div>
     )
 }
